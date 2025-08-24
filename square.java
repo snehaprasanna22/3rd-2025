d815bfb501f9b88787f74d9208b56f7869a69dde
snehaprasanna22 <snehaprasanna05@gmail.com>
2025-08-24 21:35:01 +0530

    Square of numbers

---
+++ square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class square{
+    public static void main(String[] args){
+        Scanner sc = new
+        Scanner(System.in);
+        System.out.println("Enter the number:");
+        int a = sc.nextInt();
+        int square = a*a;
+        System.out.println("The square of two numbers "+a+" is "+square);
+        sc.close();
+        }
+}
