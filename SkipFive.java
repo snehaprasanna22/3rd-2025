84da6644fedc6676ead926568c38aa4b0eb964d9
snehaprasanna22 <snehaprasanna05@gmail.com>
2025-11-14 20:33:26 +0530

    Add files via upload
    jump statement

---
+++ SkipFive.java
@@ -0,0 +1,13 @@
+public class SkipFive {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 5) {
+                continue;  // skip the iteration when i is 5
+            }
+
+            System.out.println(i);
+        }
+    }
+}
